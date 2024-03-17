5d867fabc60e3501919aeda52be9a6299b5abb46
Miguel Domingos <mighueldomingos@gmail.com>
2024-03-17 19:11:15 +0200

    style: fix wave background overflow

--- components/Pages/Components/Section/index.vue
+++ components/Pages/Components/Section/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="w-full overflow-hidden h-full py-20 relative z-0">
-    <div class="absolute w-full h-full z-[-1]">
+    <div class="absolute z-[-1]">
       <slot name="background" />
     </div>
     <slot />
